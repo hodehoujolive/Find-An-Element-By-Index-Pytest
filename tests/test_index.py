e205e421889cb2b97d1ae35d415cc44d1b80e6b4
jolive.hodehou <49763191+hodehoujolive@users.noreply.github.com>
2022-09-30 21:15:19 +0100

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -20,3 +20,18 @@ class TestIndex():
        f.write('\n')
 
     time.sleep(10)
+    
+  def test_list_country(self, driver):
+    driver.get("https://www.lambdatest.com/selenium-playground/input-form-demo")
+
+    dropdown = driver.find_element(By.NAME, "country")
+    country=dropdown.find_elements(By.XPATH, "//select[@name='country']/option")
+
+    for i in range(2,18):
+      country=dropdown.find_elements(By.XPATH, "//select[@name='country']/option")
+      country1=dropdown.find_element(By.XPATH, "//select[@name='country']/option[{}]".format(i))
+      country1.click()
+      time.sleep(2)
+      with open('list_country.txt', 'a') as f:
+        f.write(" Selected country is : "+ country1.text)
+        f.write('\n')
